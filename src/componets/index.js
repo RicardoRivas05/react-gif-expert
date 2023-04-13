66c37c6ee9ea317f1879791652a383b8b9d4ae5d
Ricardo Rivas <RicardoRivas05>
2023-04-13 07:42:46 -0600

    Primer commit

---
+++ src/componets/index.js
@@ -0,0 +1,6 @@
+//archivo de barril este sirve para no estar haciendo exportaciones
+//en todos los formularios
+
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem'
